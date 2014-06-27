454aa3ed54749649c3fe1bf11174da779b8f210a
Cavin <cstone2003@hotmail.com>
2014-06-27 18:20:50 -0500

    Add route

---
+++ Guidebook.js
@@ -0,0 +1,7 @@
+var guidebookConfig = function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'ChaptersController',
+            templateUrl: 'view/chapters.html'
+        })
+};
