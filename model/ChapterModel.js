575384abd91fdb14d3e640edad389c79fde527e9
Cavin <cstone2003@hotmail.com>
2014-06-27 19:35:47 -0500

    Add Chapter model

--- model/ChapterModel.js
+++ model/ChapterModel.js
@@ -1,3 +1,25 @@
-/**
- * Created by Stone on 6/27/14.
- */
+Guidebook.service('ChapterModel', function() {
+    this.getChapters = function() {
+        return [{
+            id: 0,
+            title: 'Chapter 1',
+            summary: 'This is chapter 1'
+        }, {
+            id: 1,
+            title: 'Chapter 2',
+            summary: 'This is chapter 2'
+        }, {
+            id: 2,
+            title: 'Chapter 3',
+            summary: 'This is chapter 3'
+        }, {
+            id: 3,
+            title: 'Chapter 4',
+            summary: 'This is chapter 4'
+        }, {
+            id: 4,
+            title: 'Chapter 5',
+            summary: 'This is chapter 5'
+        }
+        ]};
+});
