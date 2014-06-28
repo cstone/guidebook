bdf4d96752f6ae9660c3f38a607b746c24ca0b1e
Cavin <cstone2003@hotmail.com>
2014-06-27 19:36:22 -0500

    Add Add Note Controller

--- controller/NotesController.js
+++ controller/NotesController.js
@@ -1,3 +1,13 @@
-/**
- * Created by Stone on 6/27/14.
- */
+Guidebook.controller('AddNoteController',
+    function ($scope, $location, $routeParams, NoteModel) {
+        var chapterId = $routeParams.chapterId;
+        $scope.cancel = function() {
+            $location.path('/chapter/' + chapterId);
+        }
+        $scope.createNote = function() {
+            NoteModel.addNote(chapterId, $scope.note.content);
+            $location.path('/chapter/' + chapterId);
+        } }
+);
+
+
